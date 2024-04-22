ecca20dd622f1cb4eb0601e2f0482be55fd1f446
ashbur <18829526908@163.com>
2024-04-22 13:40:06 +0800

    添加 websocket 支持

--- rookie-im-tcp/src/main/java/com/rookie/im/tcp/server/ImServer.java
+++ rookie-im-tcp/src/main/java/com/rookie/im/tcp/server/ImServer.java
@@ -5,6 +5,10 @@ import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -42,6 +46,14 @@ public class ImServer {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             // 配置Channel处理器
+                            ChannelPipeline pipeline = ch.pipeline();
+                            // websocket 基于http协议，所以要有http编解码器
+                            pipeline.addLast("http-codec", new HttpServerCodec());
+                            // 对写大数据流的支持
+                            pipeline.addLast("http-chunked", new ChunkedWriteHandler());
+                            // 几乎在netty中的编程，都会使用到此hanler
+                            pipeline.addLast("aggregator", new HttpObjectAggregator(65535));
+                            pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
                         }
                     });
             ChannelFuture future = bootstrap.bind(port).sync();
